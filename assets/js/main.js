a3a335763e4dde5dd2787946ed8fe99fcd19937c
RizqiJagad <taqiyrizqijagad@gmail.com>
2024-10-20 21:25:26 +0700

    Menambahkan ScrollReveal Animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,3 +76,16 @@ const scrollActive = () =>{
 
 window.addEventListener('scroll', scrollActive)
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal('.home__social, .favorite__container')
+sr.reveal('.home__title span:nth-child(1)', {origin: 'left', opacity:1})
+sr.reveal('.home__title span:nth-child(3)', {origin: 'right', opacity:1})
+sr.reveal('.home__tooltip, .home__button', {origin: 'bottom'})
+sr.reveal('.about__data', {origin: 'left'})
+sr.reveal('.about__img', {origin:'right'})
